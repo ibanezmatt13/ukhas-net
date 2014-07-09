1081909b6e93fe1934e2dc5a74184d419019d97a
Matthew <matty.beckett@btinternet.com>
2014-07-09 18:30:35 +0100

    polished

--- node_manager.py
+++ node_manager.py
@@ -2,6 +2,7 @@ import urllib2
 from json import load
 from Tkinter import *
 
+# function called when new listbox item selected
 def onselect(event):
     w = event.widget
     index = int(w.curselection()[0])
@@ -20,23 +21,34 @@ nodeData = "http://ukhas.net/api/nodeData"
 current_id = 0
 
 window = Tk() # create window
+window.configure(bg='lightgrey')
 window.title("UKHASnet Node Manager")
+window.geometry("700x500")
 
-scrollbar = Scrollbar(window, orient="vertical")
-listNodes = Listbox(window, width=20, height=20, yscrollcommand=scrollbar.set)
+frame = Frame(window)
+frame.place(x=20, y=40)
+
+lbl1 = Label(window, text="Node List:", fg='black', bg='lightgrey', font=("Helvetica", 16, "bold"))
+lbl2 = Label(window, text="Node Information:", fg='black', bg='lightgrey', font=("Helvetica", 16,"bold"))
+lbl1.place(x=20, y=4)
+lbl2.place(x=230, y=4)
+
+listNodes = Listbox(frame, width=20, height=20, font=("Helvetica", 12))
+listNodes.pack(side="left", fill="y")
+
+scrollbar = Scrollbar(frame, orient="vertical")
 scrollbar.config(command=listNodes.yview)
 scrollbar.pack(side="right", fill="y")
 
-listSelection = Listbox(window, width=40, height=20)
+listNodes.config(yscrollcommand=scrollbar.set)
 
-# pack objects onto window
-listNodes.pack(side="left", fill="both", expand=True)
-listSelection.pack(side="left", fill="both", expand=True)
+listSelection = Listbox(window, width=50, height=4, font=("Helvetica", 12))
+listSelection.place(x=230, y=40)
 
 response = urllib2.urlopen(mapNodes)
 data = load(response)
-print data[0]
 
+# function that returns array of parsed information for certain node
 def find_info(name):
     index = 0
     for index in range(0, len(data)):
@@ -52,16 +64,16 @@ def find_info(name):
     info.append(lat)
     info.append(lon)
     return info
-    
             
-
+# populate listbox
 for current_id in range(0, len(data)):
     listNodes.insert(END, data[current_id]["name"])
 
 listNodes.bind('<<ListboxSelect>>', onselect)
 
-    
+
 mainloop()
+
 
 
 
